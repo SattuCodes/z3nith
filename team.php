0ae48689c2f54527b6fcf567e9ffe118923b4dfb
Satyam Tiwari <tiwari.sattu.911@gmail.com>
2024-05-24 17:12:38 +0530

    maam ki photo add

--- team.php
+++ team.php
@@ -16,52 +16,53 @@
             font-family: "Lato", sans-serif;
         }
         .container {
-            max-width: 1400px;
+            max-width: 1760px;
             margin: 30px 80px;
             padding: 0 20px;
         }
         .row {
             display: flex;
             justify-content: space-between;
-            margin-bottom: 20px;
+            margin-bottom: 150px;
         }
         .profile {
             position: relative;
-            width: 435px;
-            height: 435px;
+            width: 400px;
+            height: 400px;
             padding: 50px;
             display: flex;
             flex-direction: column;
             background-color: #ffffff;
             border-radius: 30px;
-            border: 1px solid #b1b8cd;
+            border: 2px solid #b1b8cd;
             justify-content: center;
             align-items: center;
             transition: height 0.3s ease;
         }
         .profile img {
-            width: 225px;
-            height: 225px;
+            width: 200px;
+            height: 200px;
             object-fit: cover;
             border-radius: 50%;
         }
         .profile h2 {
-            margin-top: 10px;
+            margin-top: 20px;
             font-size: 25px;
             font-family: 'Source Serif Pro', sans-serif;
             font-weight: 600;
             text-align: center;
         }
         .profile p {
-            margin-top: 5px;
+            margin-top: 3px;
             font-size: 20px;
             text-align: center;
         }
-        .profile .extra-info {
+        .profile .des {
             display: none;
-            margin-top: 5px;
+            margin-top: 25px;
             font-size: 18px;
             text-align: center;
+            font-style: italic;
         }
         .profile::before {
             content: '';
@@ -74,13 +75,13 @@
             transition: all 0.3s ease;
         }
         .profile:hover::before {
-            top: 30px;
-            left: 50px;
+            top: 50px;
+            left: 80px;
         }
         .profile:hover {
-            height: 500px;
+            height: 600px;
         }
-        .profile:hover .extra-info {
+        .profile:hover .des {
             display: block;
         }
     </style>
@@ -88,24 +89,64 @@
 <body>
     <?php include 'navigation.html'?>
     <div class="container">
-        <div class="row" style="margin-top: 140px">
+        <div class="row" style="margin-top: 200px">
             <div class="profile">
-                <img src="asset/img/bhavya.jpg" alt="Profile 1">
-                <h2>Bhavya Dhariwal</h2>
-                <p>Executive - Tech</p>
-                <p class="extra-info">A passionate gamer, art lover, and classical dancer who embraces technology and cherishes tradition. Organized yet clumsy, she brings creativity to everything she does</p> <!-- Extra info to display on hover -->
+                <img src="asset/img/maam.jpg" alt="Profile 1">
+                <h2>Ms. Mohini Arora</h2>
+                <p>Event Director</p>
+                <p class="des">Des</p> 
             </div>
             <div class="profile">
-                <img src="asset/img/datta.jpg" alt="Profile 2">
-                <h2>Saptangshu</h2>
-                <p>Advisor (Alumni)</p>
-                <p class="extra-info">Additional information on hover.</p> 
+                <img src="" alt="Profile 2">
+                <h2>Prjwal</h2>
+                <p>Mentor Coordinator</p>
+                <p class="des">des</p> 
             </div>
             <div class="profile">
                 <img src="profile3.jpg" alt="Profile 3">
-                <h2>Name 3</h2>
-                <p>Position 3</p>
-                <p class="extra-info">Additional information on hover.</p>
+                <h2>Abhinav Mishra</h2>
+                <p>PR Head</p>
+                <p class="des">des</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="profile">
+                <img src="" alt="Profile 1">
+                <h2>Bhavya Dhariwal</h2>
+                <p>Executive - Tech</p>
+                <p class="des">Des</p> 
+            </div>
+            <div class="profile">
+                <img src="" alt="Profile 1">
+                <h2>Maulik Pandey</h2>
+                <p>Executive - Operations</p>
+                <p class="des">Des</p> 
+            </div>
+            <div class="profile">
+                <img src="" alt="Profile 1">
+                <h2>Kabir Jhamb</h2>
+                <p>President</p>
+                <p class="des">Des</p> 
+            </div>
+        </div>
+        <div class="row">
+            <div class="profile">
+                <img src="" alt="Profile 1">
+                <h2>Sarthak Singh</h2>
+                <p>Vice President</p>
+                <p class="des">Des</p> 
+            </div>
+            <div class="profile">
+                <img src="" alt="Profile 1">
+                <h2>Aditi Gupta</h2>
+                <p>Event Lead (Uncover)</p>
+                <p class="des">Des</p> 
+            </div>
+            <div class="profile">
+                <img src="" alt="Profile 1">
+                <h2>Satyam Kumar</h2>
+                <p>Event Lead (Frame By Frame)</p>
+                <p class="des">Des</p> 
             </div>
         </div>
     </div>
