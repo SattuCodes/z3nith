34e0249903de2e4b50cdc4cfb343f1a23bf8555d
Satyam Tiwari <tiwari.sattu.911@gmail.com>
2024-05-30 11:08:53 +0530

    idk

--- index.php
+++ index.php
@@ -4,14 +4,79 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Z3NITH'24</title>
-    <link rel="stylesheet" href="style.css">
+    <style> 
+        @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+        .update {
+            margin-top: 124.078px;
+            height: 50px;
+            background-color: #333333;
+            text-align: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }   
+        .update p{ 
+            font-family: "Lato";
+            font-weight: 300;
+            font-style: normal;
+            color: white;
+        }
+        .update a{
+            font-family: "Lato", sans-serif;
+            font-weight: 700;
+            font-style: normal;
+            text-decoration: underline;
+            color: #48D597;
+        }
+        .update span{
+            color: #48D597;
+            font-size: 15px;
+        }
+        .showcase{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 80.79034028540066vh;
+            flex-direction: column;
+        }
+        .showcase img{
+            width: 14%;
+        }
+        .showcase .date{
+            padding-top: 15px;
+            text-transform: uppercase;
+            font-family: "Roboto Condensed";
+            font-size: 23px;
+            padding-bottom: 50px;
+        }
+        .vlo {
+            border-left: 3px solid black;
+            height: 500px;
+            margin-bottom: 32px;
+            margin-right: 130px;
+
+        }
+        .vlt{
+            border-left: 3px solid black;
+            height: 500px;
+            margin-left: 40px;
+        }
+    </style>
 </head>
 <body>
     <?php include "navigation.html"?>
-    <div class="start">
-        <h1>
-            Z3NITH
-        </h1>
+    <div class="update">
+        <p>
+            The Registrations for Z3NITH'24 are open, <a href="">Register Now</a><span> ></span>
+        </p>
+    </div>
+    <div class="showcase">
+        <div class="vlo"></div>
+        <img src="asset/img/with 24 (2).png" alt="Z3NITH 24 logo">
+        <p class="date">
+            26 July 2024
+        </p>
+        <div class="vlt"></div>
     </div>
 </body>
 </html>
